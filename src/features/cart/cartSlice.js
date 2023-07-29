7fec96b5731bf42c576c53be820a750524b31b26
Mike111222 <Tipu870@gmail.com>
2023-07-29 21:52:41 +0500

    Update cartSlice.js

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,17 +1,67 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-    cartItems: [],
-    amount: 0,
-    total: 0,
-    isLoading: true,
-};
+  cartItems: [ ],
+  amount: 0,
+  total: 0,
+  isLoading: true,
+}
+const url = "https://course-api.com/react-useReducer-cart-project";
 
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (name, thunkAPI) => {
+    try {
+      const resp = await axios(url);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
 
-const cartSlice = createSlice ({
-    name: 'cart',
-    initialState: 0
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {state.cartItems = []},
+    removeItem: (state, actions) => {
+       const itemId = actions.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload);
+      cartItem.amount += 1;
+    },
+    decrease : (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload);
+      cartItem.amount -= 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
+    }
+  }
 })
 
 export default cartSlice.reducer;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
