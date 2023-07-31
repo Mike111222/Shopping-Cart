c4f16dc81bee3c8e9c0512ca9d2a2a569e9dda8e
Mike111222 <Tipu870@gmail.com>
2023-07-31 20:41:20 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals, getCartItems } from './features/cart/cartSlice';
-import CartContainer from "./Components/CartContainer";
-import Navbar from "./Components/Navbar";
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
 import { useEffect } from 'react';
-import Modal from './Components/Modal'; 
+import Modal from './components/Modal'; 
 
 function App() {
   const {cartItems, isLoading} = useSelector((state) => state.cart);
